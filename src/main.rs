814db5b2bb1f0506670891da5ba60c53856440ac
Christopher League <league@contrapunctus.net>
2020-03-26 15:43:49 -0400

    Solved set_left, added set_right, fixes #1
    But still need to figure out borrowing:
    "value used here after move"

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[derive(Default)]
 
-struct Node {
+pub struct Node {
 	data: i32,
 	left: Option<Box<Node>>,
 	right: Option<Box<Node>>
@@ -21,8 +21,14 @@ impl Node {
 		}
 	}
 
-	fn set_left(&mut self, new_left: Option<Box<Node>>) -> Node {
-		Node {data: self.data, left: new_left, right: self.right.unwrap()}
+	fn set_left(&mut self, new_left: Option<Box<Node>>) -> () {
+       // If self.left was already a pointer, now we're losing it
+       // (memory leak).
+       self.left = new_left
+		// Node {data: self.data, left: new_left, right: &self.right}
+	}
+	fn set_right(&mut self, new_right: Option<Box<Node>>) -> () {
+       self.right = new_right
 	}
 
 	// fn return_left(&self) -> Option<&Self> {
@@ -60,7 +66,20 @@ fn main() {
     // let node3 = new_node(3, node4);
 
     let node4 = Node {data: 4, ..Default::default() };
-    println!("node4 val is {}", node4.print_data());
 
-    let node3 = Node {data: -3, ..Default::default() };
+    let mut node3 = Node {data: -3, ..Default::default() };
+    node3.set_left(Some(Box::new(node4)));
+
+    let node5 = Node {data: 5, ..Default::default() };
+    node3.set_right(Some(Box::new(node5)));
+
+    // We have a tree!?
+    println!("node3 val is {}", node3.print_data());
+    let ld = node3.left.unwrap().data;
+    println!("node3 left val is {}", ld);
+    println!("node3 right val is {}", node3.right.unwrap().print_data());
+
+    // Swap left and right values?
+    // let tmp = &node3.left.unwrap().data;
+    // node3.right.unwrap().data = tmp;
 }
